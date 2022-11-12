1398f733e6ef0c4a96ff5a7a0958a3fcafa47b66
da-sasaki <da-sasaki@kentem.co.jp>
2022-11-13 07:26:26 +0900

    WCFのサンプル追加

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string returnString = client.NewProc(textBox1.Text);
+
+            label1.Text = returnString;
+        }
+    }
+}
